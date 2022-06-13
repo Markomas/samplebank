087980824760769871b25ed75d94b3fccbb8aded
markom <mariukaz@gmail.com>
2022-06-13 10:25:11 +0300

    'test'

---
+++ test.py
@@ -0,0 +1,17 @@
+import csv
+from random_uk_bank_account import GenerateUkBankAccount
+
+
+sort_code = '040004'
+
+f = open('test.csv', 'w')
+writer = csv.writer(f)
+writer.writerow(['sortCode', 'accountNumber'])
+
+
+for _ in range(10):
+    account = GenerateUkBankAccount().generate_for_sort_code(sort_code)
+    writer.writerow([account.sort_code, account.account_numbers[0]])
+
+
+f.close()
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'globalcitizen/php-iban' => array(
+            'pretty_version' => 'v4.1.0',
+            'version' => '4.1.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../globalcitizen/php-iban',
+            'aliases' => array(),
+            'reference' => '5bf5d7df0bd0158460897f2b9c26eda91bbf9b7d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
