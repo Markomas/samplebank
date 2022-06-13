8be7330b342873117d29586094b64da05271618d
markom <mariukaz@gmail.com>
2022-06-13 11:10:34 +0300

    'test'

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ writer = csv.writer(f)
 writer.writerow(['sortCode', 'accountNumber'])
 
 
-for _ in range(1000):
+for _ in range(377000):
     print(_)
     account = GenerateUkBankAccount().generate_for_sort_code(sort_code)
     writer.writerow([account.sort_code, account.account_numbers[0]])
