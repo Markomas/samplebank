327ada967a625ee2c514d009c7cd819d690c5e08
markom <mariukaz@gmail.com>
2022-06-13 17:35:18 +0300

    'test'

--- test.py
+++ test.py
@@ -1,18 +1,23 @@
 import csv
+import random
+import string
 from random_uk_bank_account import GenerateUkBankAccount
 
 
 sort_code = '040004'
 
-f = open('test.csv', 'w')
+f = open('test.csv', 'w', newline='')
 writer = csv.writer(f)
-writer.writerow(['sortCode', 'accountNumber'])
+writer.writerow(['sortCode', 'accountNumber', 'email', 'phone'])
 
 
 for _ in range(377000):
     print(_)
     account = GenerateUkBankAccount().generate_for_sort_code(sort_code)
-    writer.writerow([account.sort_code, account.account_numbers[0]])
+    letters = string.ascii_lowercase
+    start = ''.join(random.choice(letters) for i in range(10))
+    phone = '44020' + str(_).zfill(6) + '22'
+    writer.writerow([account.sort_code, account.account_numbers[0], start + '@legatio.test', phone])
 
 
 f.close()
